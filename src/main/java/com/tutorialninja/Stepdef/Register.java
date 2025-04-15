fd4af662701601451feb0fe2a3fd0844205dc986
Pankaj0106 <pankajbhatt0106@gmail.com>
2025-04-15 22:40:00 +0530

    added

---
+++ src/main/java/com/tutorialninja/Stepdef/Register.java
@@ -0,0 +1,37 @@
+package com.tutorialninja.Stepdef;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.*;
+
+public class Register {
+
+	@Given("I launch the application")
+	public void i_launch_the_application() {
+		
+	}
+	
+	@Given("I navigate to Account Registration page")
+	public void i_navigate_to_account_registration_page() {
+	 
+	}
+	
+	@When("I provide all the below valid details")
+	public void i_provide_all_the_below_valid_details(DataTable dataTable) {
+
+	}
+	
+	@When("I select the Privacy Policy")
+	public void i_select_the_privacy_policy() {
+
+	}
+	
+	@When("I click on Continue Button")
+	public void i_click_on_continue_button() {
+
+	}
+	
+	@Then("I Validate the success creation")
+	public void i_validate_the_success_creation() {
+
+	}
+}
---
+++ src/main/java/com/tutorialninja/base/Base.java
@@ -0,0 +1,11 @@
+package com.tutorialninja.base;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base {
+
+	public static WebDriver driver;
+
+
+
+}
